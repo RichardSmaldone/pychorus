61bc6b221dfcbcbd5d6329e6e2c946e05439cac2
Richard Smaldone <75342083+RichardSmaldone@users.noreply.github.com>
2020-12-06 13:41:54 -0800

    Update __init__.py

--- pychorus/__init__.py
+++ pychorus/__init__.py
@@ -1,1 +1,1 @@
-from pychorus.helpers import find_and_output_chorus, find_chorus, create_chroma
+from pychorus.helpers import find_and_output_chorus, find_and_output_chorus_nparray, find_chorus, create_chroma, create_chroma_nparray
